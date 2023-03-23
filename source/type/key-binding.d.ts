58d1a2a146c40ca7e3f630bc6af5c63636f35d01
Mimiko Phonowell <phonowell@gmail.com>
2023-03-23 22:34:08 +0800

    updated

--- source/type/key-binding.d.ts
+++ source/type/key-binding.d.ts
@@ -3,13 +3,10 @@ type Key = string
 
 export class KeyBinding extends EmitterShell {
   private mapFired: Record<Name, boolean>
-  private mapGroup: Record<Name, Key[]>
-  private mapPressed: Record<Key, boolean>
-  private mapPrevented: Record<Key, boolean>
   constructor()
   private end(name: Name, key: Key): void
   private log(name: Name, key: Key, step: string, message: unknown): void
-  private registerEvent(name: Name, key: Key, isPrevented?: boolean): void
+  private registerEvent(name: Name, key: Key): void
   private start(name: Name, key: Key): void
   private unregisterEvent(name: Name, key: Key): void
 }
